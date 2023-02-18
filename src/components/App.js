603e04aa0af8e42c598d4c77f7ec846b78d3f9c0
Vishal Manjhi <vmanjhi98@gmail.com>
2023-02-19 02:11:10 +0530

    frist commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,18 @@ import '../styles/App.css';
 import { AddTodo } from './AddTodo';
 import { Todo } from './Todo';
 const App = () => {
-  const [state,dispatch] = useReducer(todoReducer,{/* Initial todo state array, could be an array for storing indivial todo objects */})
+  const [state,dispatch] = useReducer(todoReducer,[])
   
  
   return (
     <div id="main">
-      <AddTodo />
+      <AddTodo dispatch={dispatch} state={state} />
+      <Todo dispatch={dispatch} state={state}/>
      {/* Render list of Todo Components here */}
     </div>
   )
 }
+
 
 
 export default App;
