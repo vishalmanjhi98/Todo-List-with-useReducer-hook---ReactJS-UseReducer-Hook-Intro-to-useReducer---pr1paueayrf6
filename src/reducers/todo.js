889e8e3d2f54de031f7d42b10e4210a29cc6c6c9
Vishal Manjhi <vmanjhi98@gmail.com>
2023-02-19 02:14:28 +0530

    second

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,6 +1,16 @@
 const todoReducer = (state, action) => {
     switch (action.type) {
+        case"ADD":
+        return [...state,action.payload];
+        return state;
 
+        case"DELETE" :
+        return state.filter((item)=>{
+            if(item.id!=action.payload.id){
+                return state;
+            }
+        });
+        default:return state;
     }
 }
 
